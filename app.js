5d2c12c229dbd93aeccf1c6cb9883f5777a2e34f
Andrew McMillion <amcmillion3@gmail.com>
2021-08-09 10:13:17 -0400

    update work on playGame function

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const gameboard = (function () {
+    // const gameboardArray = ["", "", "", "", "", "", "", "", ""]; //Array to be used when playGame functionality works
     const gameboardArray = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
-    return {gameboardArray};
+    const render = () => {
+        for(let i = 0; i < gameboard.gameboardArray.length; i++) {
+            let gridBox = document.getElementById(`grid-${i}`);
+            gridBox.innerHTML = `<p>${gameboard.gameboardArray[i]}</p>`;
+        }
+    };
+    return {gameboardArray, render};
 })();
+
+gameboard.render(); // This to be removed once playGame functionality works
 
 const player = (name, letter) => {
     return {name, letter};
@@ -10,14 +19,12 @@ const player = (name, letter) => {
 const player1 = player("Player 1", "X");
 const player2 = player("Player 2", "O");
 
-console.log(player1);
-
-
-const render = () => {
-    for(let i = 0; i < gameboard.gameboardArray.length; i++) {
-        let gridBox = document.getElementById(`grid-${i}`);
-        gridBox.innerHTML = `<p>${gameboard.gameboardArray[i]}</p>`;
+const playGame = () => {
+    let currentPlayer = player1;
+    let letter = ""
+    const playRound = (e) => {
+        if(letter === "") {
+            let gridIndex = `e.target.id`;
+        }
     }
 }
-
-render();
